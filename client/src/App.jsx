6fc71704b6ff53e55cdc777769d9c0c67a09a7f6
Viraj Shinde <viraj100shinde@gmail.com>
2025-06-29 16:48:51 +0530

    Fix: corrected Education.jsx import path casing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import User from './User.jsx'
-import Education from './education.jsx'
+import Education from './Education.jsx'
 import Projects from './projects.jsx'
 import Skills from './Skills.jsx'
 
